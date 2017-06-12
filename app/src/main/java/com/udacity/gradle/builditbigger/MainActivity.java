a7842fa4c81327bec07cf7fe5169b63da865a15b
ebnrdwan <eng.abdoehab@gmail.com>
2017-06-12 06:58:11 +0200

    need to resolve

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,23 +1,42 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
 import com.example.JavaJoke;
 import com.example.android.andjokelib.JokeActivity;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String IntentExtraTag="INTAG";
+    public static String IntentExtraTag = "INTAG";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleApiAvailability.getInstance().makeGooglePlayServicesAvailable(MainActivity.this);
+        ClintAsync clintAsync = new ClintAsync();
+
+        new ClintAsync().execute(new Pair<Context, String>(this, "i'm the joke hehehe"));
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GoogleApiAvailability.getInstance().makeGooglePlayServicesAvailable(MainActivity.this);
     }
 
 
@@ -26,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
@@ -49,8 +69,8 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, javaJoke.getJoke(), Toast.LENGTH_SHORT).show();
         String joke = javaJoke.getJoke();
         Intent intentJoke = new Intent(this, JokeActivity.class);
-        intentJoke.putExtra(IntentExtraTag,joke);
-      startActivity(intentJoke);
+        intentJoke.putExtra(IntentExtraTag, joke);
+        startActivity(intentJoke);
 
     }
 
---
+++ app/src/main/java/com/udacity/gradle/builditbigger/getToken.java
@@ -0,0 +1,32 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Abdulrhman on 10/06/2017.
+ */
+
+public class getToken extends FirebaseInstanceIdService {
+    String TOKENFLAG;
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String token = FirebaseInstanceId.getInstance().getToken();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplication());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(TOKENFLAG,token).commit();
+
+        Log.d("TOKEN",token);
+
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
